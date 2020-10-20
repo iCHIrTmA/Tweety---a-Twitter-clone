899166631eb0a27520e2e910f82f402322000116
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-20 13:54:48 +0800

    Added create tweet functionality, avatar email skipped

--- app/Tweet.php
+++ app/Tweet.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
+		protected $guarded = [];
+		
     public function user(){
     	return $this->belongsTo(User::class);
     }
--- resources/views/_publish-tweet-panel.blade.php
+++ resources/views/_publish-tweet-panel.blade.php
@@ -1,9 +1,10 @@
 <div class="border border-blue-400 rounded-lg px-8 py-6 mb-8">
-    <form action="">
+    <form action="tweets" method="POST">
+        @csrf
         <textarea 
         name="body"
         class="w-full" 
-        placeholder="What's up doc?"  
+        placeholder="What's up doc?" 
         ></textarea>
 
         <hr class="my-4">
@@ -11,12 +12,17 @@
         <footer class="flex justify-between">
             <img 
                 src="images/default_avatar.jpg"  
-                alt="" 
+                alt="your avatar" 
                 class="rounded-full mr-2"
             >
-        <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+        <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">
+        Tweet-a-roo!
+        </button>
 
         </footer>
 
     </form>
+        @error('body')
+            <p class="text-red-500 text-sm mt-2">{{$message}}</p>
+        @enderror
 </div>

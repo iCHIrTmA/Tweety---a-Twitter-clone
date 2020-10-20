68bc33f5e16c1354f918c681735905b89050c052
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-20 18:27:31 +0800

    Everything set except from profile picture

--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -1,10 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-    <h3>profile page for {{$user->name}}</h3>
+    <header class="mb-6 relative">
+        <div class="relative">
+            <img src="{{ asset('images/default-profile-banner.jpg') }}" 
+                  alt=""
+                  class="mb-2"
+            >
 
-    <hr>
+            <img src="{{ asset('images/default-avatar.jpg') }}"
+                 alt=""
+                 class="rounded-full mr-2 absolute bottom-0 transform -translate-x-1/2 translate-y-1/2"
+                 style="left: 50%"
+                 width="150"
+            >
+        </div>
 
+        <div class="flex justify-between items-center mb-6">
+            <div style="max-width: 270px">
+                <h2 class="font-bold text-2xl mb-0">{{ $user->name }}</h2>
+                <p class="text-sm">Joined {{ $user->created_at->diffForHumans() }}</p>
+            </div>
+
+            <div class="flex">
+                {{-- @can ('edit', $user) --}}
+                    <a href=""
+                       class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2">
+                   Edit Profile</a>
+                {{-- @endcan --}}
+
+                <a href=""
+                    class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs mr-2">
+                Follow me</a>
+            </div>
+        </div>
+        <img 
+            src="{{ asset('images/default_avatar.jpg') }}"  
+            alt="your avatar" 
+            class="rounded-full mr-2 absolute"
+            {{-- style="width: 150px; "  --}}
+        >
+
+        <p class="text-sm">
+            The name’s Bugs. Bugs Bunny. Don’t wear it out. Bugs is an anthropomorphic gray
+            and white rabbit or hare who is famous for his flippant, insouciant personality.
+            He is also characterized by a Brooklyn accent, his portrayal as a trickster,
+            and his catch phrase "Eh...What's up, doc?"
+        </p>
+
+
+    </header>
+    
     @include('_timeline', [
         'tweets' => $user->tweets
     ])

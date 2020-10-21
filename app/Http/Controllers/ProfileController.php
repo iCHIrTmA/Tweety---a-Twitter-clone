405ca9c7a4946c7f3c06f838ebfa4b6e2fb652bd
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-21 19:05:38 +0800

    Made UserPolicy added policy in routes

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,6 +11,8 @@ class ProfileController extends Controller
   	return view('profiles.show', compact('user'));
   }
   public function edit(User $user){
+  	// abort_if($user->isNot(current_user()), 403);
+  	// $this->authorize('edit', $user);
   	return view('profiles.edit', compact('user'));
   }
 
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -23,14 +23,14 @@
             </div>
 
             <div class="flex">
-                @if(current_user()->is($user))
-                {{-- @can ('edit', $user) --}}
+                {{-- @if(current_user()->is($user)) --}} 
+                @can ('edit', $user)
                     <a href="{{$user->path('edit')}}"
                        class="rounded-full border border-gray-300 py-2 px-4 text-black text-xs mr-2">
                    Edit Profile
                     </a>
-                {{-- @endcan --}}
-                @endif
+                @endcan
+                {{-- @endif --}}
                 <x-follow-button :user="$user"></x-follow-button>
 
             </div>

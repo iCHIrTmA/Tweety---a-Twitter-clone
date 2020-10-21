7d039074c70ca66420a9817e8a751602791d2c4e
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-21 18:35:10 +0800

    added helpers.php in composer.json

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,11 +1,11 @@
-@if(auth()->user()->isNot($user))
+@if(current_user()->isNot($user))
 <form method="POST" action="{{url('profiles/'. $user->name .'/follow')}}">
     @csrf
     <button
         type="submit"
         class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs mr-2"
     >
-    {{auth()->user()->following($user) ? 'Unfollow me' : 'Follow me'}}
+    {{current_user()->following($user) ? 'Unfollow me' : 'Follow me'}}
     </button>
 </form>
 @endif

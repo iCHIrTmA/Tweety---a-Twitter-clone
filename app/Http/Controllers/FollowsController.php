73fb350a011d9cea0d5adbddecde810a9d6e49d6
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-21 17:25:35 +0800

    Added follow toggle logic in FollowsController

--- app/Http/Controllers/FollowsController.php
+++ app/Http/Controllers/FollowsController.php
@@ -7,8 +7,12 @@ use Illuminate\Http\Request;
 
 class FollowsController extends Controller
 {
-    public function store(User $user){
-    	auth()->user()->follow($user);
-    	return back();
-    }
+  public function store(User $user){
+  	if(auth()->user()->following($user)){
+  		auth()->user()->unfollow($user);
+  	}else{
+  	auth()->user()->follow($user);
+  	}
+  	return back();
+  }
 }

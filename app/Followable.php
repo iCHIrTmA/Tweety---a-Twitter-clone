1feed1d29d69581f0cd6d3ccc53accce98646afb
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-21 17:31:51 +0800

    Transferred togglefollow logic from FollowsController to Followable trait

--- app/Followable.php
+++ app/Followable.php
@@ -11,7 +11,14 @@ trait Followable
 
   public function unfollow(User $user){
     return $this->follows()->detach($user);
-}
+	}
+
+	  public function toggleFollow(User $user){
+	  	if($this->following($user)){
+	  		return $this->unfollow($user);
+	  	}
+	  		return $this->follow($user);  
+	  }
 
   public function follows(){
       return $this->belongsToMany(User::class, 'follows', 'user_id', 'following_user_id');

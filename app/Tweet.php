23292ea91cb1c787d466bcb46ca8e267fd8f8fdb
Janroe Cordero <jcdelloro@up.edu.ph>
2020-10-22 16:05:58 +0800

    made Like model and Likable trait

--- app/Tweet.php
+++ app/Tweet.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
+		use Likable;
 		protected $guarded = [];
 		
     public function user(){
     	return $this->belongsTo(User::class);
     }
+
 }
